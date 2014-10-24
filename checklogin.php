2ed76a6f20c6275069a79b2246ba3d3c8b9a99f1
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-10-24 09:54:25 +0100

    changes

--- checklogin.php
+++ checklogin.php
@@ -36,7 +36,8 @@ session_start();
 // Register $myusername, $mypassword and redirect to file "customer_area.php"
 $_SESSION["username"] = $username;
 $_SESSION["password"] = $password;
-header("location:querylist.php");
+echo $num_rows . ' ' . $username . ' ' . $password;
+//header("location:querylist.php");
 }
 else {
 echo "Wrong Username or Password";
