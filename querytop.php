8ee44d2d0a7afd43d828d63687895d599a95cd64
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-06 17:38:57 +0000

    added ajax call

--- querytop.php
+++ querytop.php
@@ -14,7 +14,8 @@ include 'config.php' ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-      <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <title>Prototype</title>
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
@@ -27,7 +28,7 @@ include 'config.php' ?>
 
     <!-- Custom styles for this template -->
     <link href="starter-template.css" rel="stylesheet">
-<script type="javascript">
+<script>
     $("#submit").click(function(){
         alert("button clicked");
 
