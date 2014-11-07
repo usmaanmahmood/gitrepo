32d2229fd0fa4ed7495db6bfa1cf09a03070f7f7
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-07 16:50:12 +0000

    script auto commit

--- ArcadeQuery.php
+++ ArcadeQuery.php
@@ -15,10 +15,10 @@ class Filter {
 
     public function __construct($inDatabase, $inGroup, $inStudentUsername, $inStudentFullname, $inModule) {
         if (empty($inDatabase)) $this->database = " "; else $this->database = $inDatabase;
-        if (empty($inGroup)) $this->$group = " "; else $this->$group = $inGroup;
-        if (empty($inStudentUsername)) $this->$studentUsername = " "; else $this->$studentUsername = $inStudentUsername;
-        if (empty($inStudentFullname)) $this->$studentFullname = " "; else $this->$studentFullname = $inStudentFullname;
-        if (empty($inModule)) $this->$module = " "; else $this->$module = $inModule;
+        if (empty($inGroup)) $this->group = " "; else $this->group = $inGroup;
+        if (empty($inStudentUsername)) $this->studentUsername = " "; else $this->studentUsername = $inStudentUsername;
+        if (empty($inStudentFullname)) $this->studentFullname = " "; else $this->studentFullname = $inStudentFullname;
+        if (empty($inModule)) $this->module = " "; else $this->module = $inModule;
     }
 
     //getters
