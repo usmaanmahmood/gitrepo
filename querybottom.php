c8dbc3fec907465eb2f938f3922bd2abd244e29d
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-09 19:28:06 +0000

    script auto commit

--- querybottom.php
+++ querybottom.php
@@ -53,27 +53,30 @@
     $("#DatabaseList").click(function() {
         var $databaseList = $("#DatabaseList");
         var $groupList = $("#GroupList");
+        var $studentList = $("#StudentList");
         var $moduleList = $("#ModuleList");
         var $selectedDatabases = $databaseList.val();
 
         $groupList.empty();
+        $studentList.empty();
         $moduleList.empty();
 
 
         var $stringGroupsList = [];
         var $stringGroupsAdded = [];
 
+        var $stringStudentsList = [];
+        var $stringStudentsAdded = [];
+
         var $stringModulesList = [];
         var $stringModulesAdded = [];
-
-
-
 
 
         for (var i = 0; i < $json.length; i++) {   // if nothing is selected, show all the modules. if the one that is selected = current filter, then that filter is visible.
             if (($selectedDatabases == null) || ($.inArray($json[i].database, $selectedDatabases) > -1)) {
                 $json[i].visible = true;
                 $stringGroupsList.push($json[i].group);
+                $stringStudentsList.push($json[i].studentUsername);
                 $stringModulesList.push($json[i].module);
             }
         }
@@ -82,6 +85,13 @@
             if ($.inArray(value, $stringGroupsAdded) == -1) {
                 $groupList.append("<option value=\"" + value + "\">" + value + "</option>");
                 $stringGroupsAdded.push(value);
+            }
+        });
+
+        $.each($stringStudentsList, function(key, value) {
+            if ($.inArray(value, $stringStudentsAdded) == -1) {
+                $studentList.append("<option value=\"" + value + "\">" + value + "</option>");
+                $stringStudentsAdded.push(value);
             }
         });
 
