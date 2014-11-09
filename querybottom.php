9bc0581215308794fb3a2c3612b9f14940b7a3dd
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-09 17:49:26 +0000

    script auto commit

--- querybottom.php
+++ querybottom.php
@@ -59,15 +59,33 @@
 
         for (var i = 0; i < $json.length; i++)
         {
-            $json[i].visible = ($.inArray($json[i].database, $selectedDatabases) > -1); // if brackets is true (found), then its set to ture
+            $json[i].visible =  ($.inArray($json[i].database, $selectedDatabases) > -1) ||
+                                ($selectedDatabases == null) ||
+
+                                ($.inArray($json[i].group, $selectedGroups) > -1) ||
+                                ($selectedGroups == null) ||
+
+                                ($.inArray($json[i].studentUsername, $selectedUsernames) > -1) ||
+                                ($selectedUsernames == null) ||
+
+                                ($.inArray($json[i].module, $selectedModules) > -1) ||
+                                ($selectedModules == null)
+                                ;
+
         }
 
+        $('#DatabaseList').empty();
+        $('#GroupList').empty();
+        $('#StudentList').empty();
         $('#ModuleList').empty();
 
         for (var i = 0; i < $json.length; i++)
         {
             if ($json[i].visible)
             {
+                $('#DatabaseList').append("<option>" + $json[i].database + "</option>");
+                $('#GroupList').append("<option>" + $json[i].group + "</option>");
+                $('#StudentList').append("<option>" + $json[i].studentUsername + "</option>");
                 $('#ModuleList').append("<option>" + $json[i].module + "</option>");
             }
         }
