a47e7b307f59c0afc5eb6058d17d8e656dbe867b
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-09 18:55:25 +0000

    script auto commit

--- querybottom.php
+++ querybottom.php
@@ -62,7 +62,7 @@
         for (var i = 0; i < $json.length; i++) {   // if nothing is selected, show all the modules. if the one that is selected = current filter, then that filter is visible.
             if (($selectedDatabases == null) || ($.inArray($json[i].database, $selectedDatabases) > -1)) {
                 $json[i].visible = true;
-                $moduleList.append("<option value=\"" + $json[i].group + "\">" + $json[i].group + "</option>");
+                $groupList.append("<option value=\"" + $json[i].group + "\">" + $json[i].group + "</option>");
                 $moduleList.append("<option value=\"" + $json[i].module + "\">" + $json[i].module + "</option>");
             }
         };
