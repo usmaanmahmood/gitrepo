43f496e0d1e253312703d1c30f788e727e4b4fde
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-09 13:35:42 +0000

    script auto commit

--- testing.php
+++ testing.php
@@ -212,7 +212,7 @@ $arcadeProfile = new ArcadeProfile($arcadeProfileString);
 echo $_SESSION['username'] . $_SESSION['arcadepassword'] . '<br/>';
 ?>
 
-
+<h1>change</h1>
 <div class="row">
     <div class="col-md-4">
         <p>Command</p>
