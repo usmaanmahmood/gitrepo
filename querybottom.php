01ae3f1a1932f7f749fc776ecb64b254ebc15e63
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-09 18:09:30 +0000

    script auto commit

--- querybottom.php
+++ querybottom.php
@@ -59,17 +59,13 @@
 
         for (var i = 0; i < $json.length; i++)
         {
-            $json[i].visible =  ($.inArray($json[i].database, $selectedDatabases) > -1) ||
-                                ($selectedDatabases == null) ||
+            $json[i].visible =  ($.inArray($json[i].database, $selectedDatabases) > -1) || ($selectedDatabases == null) ||
 
-                                ($.inArray($json[i].group, $selectedGroups) > -1) ||
-                                ($selectedGroups == null) ||
+                                ($.inArray($json[i].group, $selectedGroups) > -1) || ($selectedGroups == null) ||
 
-                                ($.inArray($json[i].studentUsername, $selectedUsernames) > -1) ||
-                                ($selectedUsernames == null) ||
+                                ($.inArray($json[i].studentUsername, $selectedUsernames) > -1) || ($selectedUsernames == null) ||
 
-                                ($.inArray($json[i].module, $selectedModules) > -1) ||
-                                ($selectedModules == null)
+                                ($.inArray($json[i].module, $selectedModules) > -1) || ($selectedModules == null)
                                 ;
 
         }
@@ -89,9 +85,9 @@
             if ($json[i].visible)
             {
 //                if ($.inArray($json[i].database, $uniqueDatabases) == -1) { $uniqueDatabases.push($json[i].database); }
-                if ($.inArray($json[i].group, $uniqueGroups) == -1) { $uniqueGroups.push($json[i].group); }
-                if ($.inArray($json[i].studentUsername, $uniqueStudentUsernames) == -1) { $uniqueStudentUsernames.push($json[i].studentUsername); }
-                if ($.inArray($json[i].module, $uniqueModules) == -1) { $uniqueModules.push($json[i].module); }
+                if ($.inArray($json[i].group, $uniqueGroups) == -1) || ($selectedGroups == null) { $uniqueGroups.push($json[i].group); }
+                if ($.inArray($json[i].studentUsername, $uniqueStudentUsernames) == -1) || ($selectedUsernames == null) { $uniqueStudentUsernames.push($json[i].studentUsername); }
+                if ($.inArray($json[i].module, $uniqueModules) == -1) || ($selectedModules == null) { $uniqueModules.push($json[i].module); }
             }
         }
 
