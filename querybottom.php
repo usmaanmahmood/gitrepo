7c841aaf75354e9a9e4cb2b765170630294f051f
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-09 16:58:50 +0000

    script auto commit

--- querybottom.php
+++ querybottom.php
@@ -1,4 +1,4 @@
-<table id="target_table_id">
+<table id="table">
     <tbody></tbody>
 </table>
       </div>
@@ -59,7 +59,17 @@
         });
     });
 
-    
+    $.getJSON('getFilterLists.php',
+        function (json) {
+            var tr;
+            for (var i = 0; i < json.length; i++) {
+                tr = $('<tr/>');
+                tr.append("<td>" + json[i].User_Name + "</td>");
+                tr.append("<td>" + json[i].score + "</td>");
+                tr.append("<td>" + json[i].team + "</td>");
+                $('#table').append(tr);
+            }
+        });
 
 
 </script>
