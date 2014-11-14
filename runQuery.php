2fc07805b343014584c898db9a23509b834d4d11
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-14 18:44:34 +0000

    script auto commit

--- runQuery.php
+++ runQuery.php
@@ -6,21 +6,6 @@
  * Time: 17:29
  */
 
-//include "ArcadeQuery.php";
-//
-//$command =
-//$databases = (($_GET["databases"] == null) ? " " : rtrim(implode(" ", $_GET["databases"])));
-//$groups = (($_GET["groups"] == null) ? " " : rtrim(implode(" ", $_GET["groups"])));
-//$students = (($_GET["students"] == null) ? " " : rtrim(implode(" ", $_GET["students"])));
-//$modules = (($_GET["modules"] == null) ? " " : rtrim(implode(" ", $_GET["modules"])));
-//
-//$arcadeQuery = new ArcadeQuery($command);
-//
-//$arcadeQuery->addFilter(new Filter($databases, $groups, $students, "", $modules));
-//
-//$arcadeQuery->sendQuery();
-//
-//echo $arcadeQuery->getResult();
 include("config.php");
 
 $arcadeClient = new ARCADEClient();
