78d8be10aaf36558cf8ae9504737cd1ef7467362
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-14 19:16:03 +0000

    script auto commit

--- querybottom.php
+++ querybottom.php
@@ -65,63 +65,63 @@
 
     var $json;
 
-//    $.getJSON ('getFilterLists.php', function (json) {
-//        for (var i = 0; i < json.length; i++) { json[i].visible = false; }
-//        $json = json;
-//    });
+    $.getJSON ('getFilterLists.php', function (json) {
+        for (var i = 0; i < json.length; i++) { json[i].visible = false; }
+        $json = json;
+    });
 
-//    $("#DatabaseList").click(function() {
-//        var $databaseList = $("#DatabaseList");
-//        var $selectedDatabases = $databaseList.val();
-//
-//        var $groupList = $("#GroupList");
-//        var $studentList = $("#StudentList");
-//        var $moduleList = $("#ModuleList");
-//
-//        $groupList.empty();
-//        $studentList.empty();
-//        $moduleList.empty();
-//
-//        var $stringGroupsList = [];
-//        var $stringGroupsAdded = [];
-//
-//        var $stringStudentsList = [];
-//        var $stringStudentsAdded = [];
-//
-//        var $stringModulesList = [];
-//        var $stringModulesAdded = [];
-//
-//
-//        for (var i = 0; i < $json.length; i++) {   // if nothing is selected, show all the modules. if the one that is selected = current filter, then that filter is visible.
-//            if (($selectedDatabases == null) || ($.inArray($json[i].database, $selectedDatabases) > -1)) {
-//                $json[i].visible = true;
-//                $stringGroupsList.push($json[i].group);
-//                $stringStudentsList.push($json[i].studentUsername);
-//                $stringModulesList.push($json[i].module);
-//            }
-//        }
-//
-//        $.each($stringGroupsList, function(key, value) {
-//            if ($.inArray(value, $stringGroupsAdded) == -1) {
-//                $groupList.append("<option value=\"" + value + "\">" + value + "</option>");
-//                $stringGroupsAdded.push(value);
-//            }
-//        });
-//
-//        $.each($stringStudentsList, function(key, value) {
-//            if ($.inArray(value, $stringStudentsAdded) == -1) {
-//                $studentList.append("<option value=\"" + value + "\">" + value + "</option>");
-//                $stringStudentsAdded.push(value);
-//            }
-//        });
-//
-//        $.each($stringModulesList, function(key, value) {
-//            if ($.inArray(value, $stringModulesAdded) == -1) {
-//                $moduleList.append("<option value=\"" + value + "\">" + value + "</option>");
-//                $stringModulesAdded.push(value);
-//            }
-//        });
-//    });
+    $("#DatabaseList").click(function() {
+        var $databaseList = $("#DatabaseList");
+        var $selectedDatabases = $databaseList.val();
+
+        var $groupList = $("#GroupList");
+        var $studentList = $("#StudentList");
+        var $moduleList = $("#ModuleList");
+
+        $groupList.empty();
+        $studentList.empty();
+        $moduleList.empty();
+
+        var $stringGroupsList = [];
+        var $stringGroupsAdded = [];
+
+        var $stringStudentsList = [];
+        var $stringStudentsAdded = [];
+
+        var $stringModulesList = [];
+        var $stringModulesAdded = [];
+
+
+        for (var i = 0; i < $json.length; i++) {   // if nothing is selected, show all the modules. if the one that is selected = current filter, then that filter is visible.
+            if (($selectedDatabases == null) || ($.inArray($json[i].database, $selectedDatabases) > -1)) {
+                $json[i].visible = true;
+                $stringGroupsList.push($json[i].group);
+                $stringStudentsList.push($json[i].studentUsername);
+                $stringModulesList.push($json[i].module);
+            }
+        }
+
+        $.each($stringGroupsList, function(key, value) {
+            if ($.inArray(value, $stringGroupsAdded) == -1) {
+                $groupList.append("<option value=\"" + value + "\">" + value + "</option>");
+                $stringGroupsAdded.push(value);
+            }
+        });
+
+        $.each($stringStudentsList, function(key, value) {
+            if ($.inArray(value, $stringStudentsAdded) == -1) {
+                $studentList.append("<option value=\"" + value + "\">" + value + "</option>");
+                $stringStudentsAdded.push(value);
+            }
+        });
+
+        $.each($stringModulesList, function(key, value) {
+            if ($.inArray(value, $stringModulesAdded) == -1) {
+                $moduleList.append("<option value=\"" + value + "\">" + value + "</option>");
+                $stringModulesAdded.push(value);
+            }
+        });
+    });
 
 
 
