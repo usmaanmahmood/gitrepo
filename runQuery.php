61f6db8846eda3ee4e86670c21f6cec7643d48c8
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-14 18:50:20 +0000

    script auto commit

--- runQuery.php
+++ runQuery.php
@@ -12,18 +12,15 @@ $arcadeClient = new ARCADEClient();
 $query = new Query($_GET["command"], 1); // command, plainTextWanted
 
 // if empty, send empty array through instead of null
-echo var_dump($_GET["databases"]);
-echo var_dump($_GET["modules"]);
 
-echo (empty($_GET["modules"]) ? "yes" : "no");
-//
-//if (!empty($_GET["databases"])) $query->addDatabases(implode(" ", $_GET["databases"]));
-//
-//if (!empty($_GET["groups"]))    $query->addGroups(implode(" ", $_GET["groups"]));
-//
-//if (!empty($_GET["students"]))  $query->addStudents(implode(" ", $_GET["students"]));
-//
-//if (!empty($_GET["modules"]))   $query->addModules(implode(" ", $_GET["modules"]));
+// if none selected then it is empty.
+if (!empty($_GET["databases"])) $query->addDatabases($_GET["databases"]);
+
+if (!empty($_GET["groups"]))    $query->addGroups($_GET["groups"]);
+
+if (!empty($_GET["students"]))  $query->addStudents($_GET["students"]);
+
+if (!empty($_GET["modules"]))   $query->addModules($_GET["modules"]);
 
 //$plainResult = $arcadeClient->execute($query);
 
