c272a2ecc244cb3b1e6f5846f7d9757b02c1bb9e
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-16 18:38:56 +0000

    script auto commit

--- Login.php
+++ Login.php
@@ -22,6 +22,8 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <title>Login | ARCADE</title>
     <style>
         body {
             padding-top: 40px;
@@ -63,13 +65,7 @@
             border-top-left-radius: 0;
             border-top-right-radius: 0;
         }
-
     </style>
-<title>Login | ARCADE</title>
-<style>
-    <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
-</style>
 </head>
 
 <body>
