34a6aa7d53729c5e7a1cd8fb5c88f7994cf6a4f7
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-16 19:05:03 +0000

    script auto commit

--- template-nav.php
+++ template-nav.php
@@ -16,7 +16,7 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li>
-                    <a href="display/Classic.php">Classic ARCADE</a>
+                    <a href="./display/Classic.php">Classic ARCADE</a>
                 </li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Advanced Displays<span class="caret"></span></a>
