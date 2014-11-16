156dc7841ce588695b2718b7c0eb1bb005f3709a
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-16 17:36:33 +0000

    script auto commit

--- querybottom.php
+++ querybottom.php
@@ -26,6 +26,7 @@ $( document ).ready(function() {
 
         // validation
         if ($command == null || $command == "") {
+            $('#queryWarning').fadeOut('slow');
             $('#queryWarning').html("<div class=\"alert alert-danger\" role=\"alert\">Please select a command.</div>");
             $('#queryWarning').fadeIn('slow');
             return;
