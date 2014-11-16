c3d28bb45fad0d5e267935fcf632b9d512058ce1
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-16 18:12:01 +0000

    script auto commit

--- Classic.php
+++ Classic.php
@@ -80,6 +80,9 @@
 
 </div><!-- /.container -->
 
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script>
 $(document).ready(function () {
     $('#resultspane').hide();
