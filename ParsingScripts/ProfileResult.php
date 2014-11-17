763b9aa94dc907449bbf1a2b1d29d180ccdd2577
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-17 17:11:11 +0000

    script auto commit

--- ParsingScripts/ProfileResult.php
+++ ParsingScripts/ProfileResult.php
@@ -55,7 +55,7 @@ class ProfileResult extends Result
 
     public function getDatabaseList()
     {
-        return $this->databaseList;
+        return array_unique($this->databaseList);
     }
 
     public function getGroupList()
