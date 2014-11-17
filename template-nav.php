c827caf11436e7dee25401a820631ed8e491e695
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-17 10:55:29 +0000

    script auto commit

--- template-nav.php
+++ template-nav.php
@@ -22,8 +22,31 @@
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Advanced Displays<span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
                         <li><a href="Registration-Details.php">Registration Details</a></li>
+                        <li>registration-details: with lab groups</li>
                         <li class="divider"></li>
-                        <li><a href="#">Separated link</a></li>
+                        <li>display-picture</li>
+                        <li class="divider"></li>
+                        <li>time-table: remaining</li>
+                        <li>time-table: full</li>
+                        <li>time-table: with extensions</li>
+                        <li class="divider"></li>
+                        <li>attendance-summary</li>
+                        <li class="divider"></li>
+                        <li>marks-table: all</li>
+                        <li>marks-table: fails only</li>
+                        <li>marks-table: ignore empty columns</li>
+                        <li class="divider"></li>
+                        <li>expected</li>
+                        <li class="divider"></li>
+                        <li>excuses</li>
+                        <li class="divider"></li>
+                        <li>irregularities</li>
+                        <li class="divider"></li>
+                        <li>failure-predictions</li>
+                        <li class="divider"></li>
+                        <li>full-story: with notes</li>
+                        <li>full-story: without notes</li>
+                        <li>full-story: chronological</li>
                     </ul>
                 </li>
                 <li>
