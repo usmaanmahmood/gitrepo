0f34fb9eb09877a66be87edf2a281fbab5447cd4
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-17 11:11:40 +0000

    script auto commit

---
+++ ParsingScripts/RegistrationDetailsParser.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mmapxum2
+ * Date: 17/11/14
+ * Time: 11:07
+ */ 
--- rawprofile.php
+++ rawprofile.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP); // create new socket
-socket_connect ($socket , "carousel", 4000); // connect to arcade on port 4000
+socket_connect($socket, "carousel", 4000); // connect to arcade on port 4000
 
 $username = "mahmoou1";
 $arcadepassword = "LQKUGRIRDE";
@@ -17,13 +17,13 @@ $arcadepassword = "LQKUGRIRDE";
 socket_write($socket, "LKJHGFDSA\n"); // hello token
 socket_write($socket, $username . "\n"); // arcade user
 socket_write($socket, $arcadepassword . "\n"); // arcade pass
-$fullquery = "profile" . "\n" . "" . "\n" . "" . "\n" . "" . "\n" . "" . "\n";
+$fullquery = "registration-details:" . "\n" . "12-13-2X 12-13-2 11-12-1X 11-12-1" . "\n" . "" . "\n" . "" . "\n" . "" . "\n";
 socket_write($socket, $fullquery); // this sends it off
 
 $results = "";
 
 // accept data until remote host closes the connection
-while ($socketoutput = socket_read($socket, "100000"))	{
+while ($socketoutput = socket_read($socket, "100000")) {
     if ($socketoutput != "++WORKING\n")
         $results .= $socketoutput;
 }
@@ -32,10 +32,10 @@ while ($socketoutput = socket_read($socket, "100000"))	{
 socket_shutdown($socket, 2); // 2 = shutdown reading and writing
 socket_close($socket);
 
-
-include("ProfileParser.php");
-
-$profileParser = new ProfileParser();
-$profile = $profileParser->parse($results);
-
-echo var_dump(array_unique($profile->getDatabaseList()));
+echo $results
+//include("ProfileParser.php");
+//
+//$profileParser = new ProfileParser();
+//$profile = $profileParser->parse($results);
+//
+//echo var_dump(array_unique($profile->getDatabaseList()));
