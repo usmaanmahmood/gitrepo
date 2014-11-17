d4ba25e4748c1f4a6aa877ba415a36fb2d583397
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-17 17:43:20 +0000

    script auto commit

--- display/Registration-Details.php
+++ display/Registration-Details.php
@@ -41,7 +41,7 @@ $databaseList = $result->getDatabaseList();
                 <?php
                 foreach ($databaseList as $key => $database) {
                     ?>
-                    <h3>Database: <?php $database["databaseName"] ?><pre><?php var_dump($database) ?></pre></h3>
+                    <h3>Database: <?php ; ?><pre><?php var_dump($database->getDatabaseName()) ?></pre></h3>
                 <?php
                 }
                 ?>
