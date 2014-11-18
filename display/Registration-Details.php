4ce0e8d709c851fc8350812927f3ce6240602174
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-18 11:08:21 +0000

    script auto commit

--- display/Registration-Details.php
+++ display/Registration-Details.php
@@ -40,7 +40,7 @@
                     <?php
                     foreach ($databaseList as $key => $database) {
                         $key1 = $key + 1;
-                        $currentNumber = ucfirst(convertNumber(($key1)));
+                        $currentNumber = ucfirst(convert_number_to_words(($key1)));
 
                         ?>
                         <div class="panel panel-default">
@@ -146,203 +146,116 @@
     </body>
     </html>
 <?php
-function convertNumber($number)
-{
-    list($integer, $fraction) = explode(".", (string)$number);
+function convert_number_to_words($number) {
 
-    $output = "";
+    $hyphen      = '-';
+    $conjunction = ' and ';
+    $separator   = ', ';
+    $negative    = 'negative ';
+    $decimal     = ' point ';
+    $dictionary  = array(
+        0                   => 'zero',
+        1                   => 'one',
+        2                   => 'two',
+        3                   => 'three',
+        4                   => 'four',
+        5                   => 'five',
+        6                   => 'six',
+        7                   => 'seven',
+        8                   => 'eight',
+        9                   => 'nine',
+        10                  => 'ten',
+        11                  => 'eleven',
+        12                  => 'twelve',
+        13                  => 'thirteen',
+        14                  => 'fourteen',
+        15                  => 'fifteen',
+        16                  => 'sixteen',
+        17                  => 'seventeen',
+        18                  => 'eighteen',
+        19                  => 'nineteen',
+        20                  => 'twenty',
+        30                  => 'thirty',
+        40                  => 'fourty',
+        50                  => 'fifty',
+        60                  => 'sixty',
+        70                  => 'seventy',
+        80                  => 'eighty',
+        90                  => 'ninety',
+        100                 => 'hundred',
+        1000                => 'thousand',
+        1000000             => 'million',
+        1000000000          => 'billion',
+        1000000000000       => 'trillion',
+        1000000000000000    => 'quadrillion',
+        1000000000000000000 => 'quintillion'
+    );
 
-    if ($integer{0} == "-") {
-        $output = "negative ";
-        $integer = ltrim($integer, "-");
-    } else if ($integer{0} == "+") {
-        $output = "positive ";
-        $integer = ltrim($integer, "+");
+    if (!is_numeric($number)) {
+        return false;
     }
 
-    if ($integer{0} == "0") {
-        $output .= "zero";
-    } else {
-        $integer = str_pad($integer, 36, "0", STR_PAD_LEFT);
-        $group = rtrim(chunk_split($integer, 3, " "), " ");
-        $groups = explode(" ", $group);
+    if (($number >= 0 && (int) $number < 0) || (int) $number < 0 - PHP_INT_MAX) {
+        // overflow
+        trigger_error(
+            'convert_number_to_words only accepts numbers between -' . PHP_INT_MAX . ' and ' . PHP_INT_MAX,
+            E_USER_WARNING
+        );
+        return false;
+    }
 
-        $groups2 = array();
-        foreach ($groups as $g) {
-            $groups2[] = convertThreeDigit($g{0}, $g{1}, $g{2});
-        }
+    if ($number < 0) {
+        return $negative . convert_number_to_words(abs($number));
+    }
 
-        for ($z = 0; $z < count($groups2); $z++) {
-            if ($groups2[$z] != "") {
-                $output .= $groups2[$z] . convertGroup(11 - $z) . (
-                    $z < 11
-                    && !array_search('', array_slice($groups2, $z + 1, -1))
-                    && $groups2[11] != ''
-                    && $groups[11]{0} == '0'
-                        ? " and "
-                        : ", "
-                    );
+    $string = $fraction = null;
+
+    if (strpos($number, '.') !== false) {
+        list($number, $fraction) = explode('.', $number);
+    }
+
+    switch (true) {
+        case $number < 21:
+            $string = $dictionary[$number];
+            break;
+        case $number < 100:
+            $tens   = ((int) ($number / 10)) * 10;
+            $units  = $number % 10;
+            $string = $dictionary[$tens];
+            if ($units) {
+                $string .= $hyphen . $dictionary[$units];
             }
+            break;
+        case $number < 1000:
+            $hundreds  = $number / 100;
+            $remainder = $number % 100;
+            $string = $dictionary[$hundreds] . ' ' . $dictionary[100];
+            if ($remainder) {
+                $string .= $conjunction . convert_number_to_words($remainder);
+            }
+            break;
+        default:
+            $baseUnit = pow(1000, floor(log($number, 1000)));
+            $numBaseUnits = (int) ($number / $baseUnit);
+            $remainder = $number % $baseUnit;
+            $string = convert_number_to_words($numBaseUnits) . ' ' . $dictionary[$baseUnit];
+            if ($remainder) {
+                $string .= $remainder < 100 ? $conjunction : $separator;
+                $string .= convert_number_to_words($remainder);
+            }
+            break;
+    }
+
+    if (null !== $fraction && is_numeric($fraction)) {
+        $string .= $decimal;
+        $words = array();
+        foreach (str_split((string) $fraction) as $number) {
+            $words[] = $dictionary[$number];
         }
-
-        $output = rtrim($output, ", ");
+        $string .= implode(' ', $words);
     }
 
-    if ($fraction > 0) {
-        $output .= " point";
-        for ($i = 0; $i < strlen($fraction); $i++) {
-            $output .= " " . convertDigit($fraction{$i});
-        }
-    }
-
-    return $output;
-}
-
-function convertGroup($index)
-{
-    switch ($index) {
-        case 11:
-            return " decillion";
-        case 10:
-            return " nonillion";
-        case 9:
-            return " octillion";
-        case 8:
-            return " septillion";
-        case 7:
-            return " sextillion";
-        case 6:
-            return " quintrillion";
-        case 5:
-            return " quadrillion";
-        case 4:
-            return " trillion";
-        case 3:
-            return " billion";
-        case 2:
-            return " million";
-        case 1:
-            return " thousand";
-        case 0:
-            return "";
-    }
-}
-
-function convertThreeDigit($digit1, $digit2, $digit3)
-{
-    $buffer = "";
-
-    if ($digit1 == "0" && $digit2 == "0" && $digit3 == "0") {
-        return "";
-    }
-
-    if ($digit1 != "0") {
-        $buffer .= convertDigit($digit1) . " hundred";
-        if ($digit2 != "0" || $digit3 != "0") {
-            $buffer .= " and ";
-        }
-    }
-
-    if ($digit2 != "0") {
-        $buffer .= convertTwoDigit($digit2, $digit3);
-    } else if ($digit3 != "0") {
-        $buffer .= convertDigit($digit3);
-    }
-
-    return $buffer;
-}
-
-function convertTwoDigit($digit1, $digit2)
-{
-    if ($digit2 == "0") {
-        switch ($digit1) {
-            case "1":
-                return "ten";
-            case "2":
-                return "twenty";
-            case "3":
-                return "thirty";
-            case "4":
-                return "forty";
-            case "5":
-                return "fifty";
-            case "6":
-                return "sixty";
-            case "7":
-                return "seventy";
-            case "8":
-                return "eighty";
-            case "9":
-                return "ninety";
-        }
-    } else if ($digit1 == "1") {
-        switch ($digit2) {
-            case "1":
-                return "eleven";
-            case "2":
-                return "twelve";
-            case "3":
-                return "thirteen";
-            case "4":
-                return "fourteen";
-            case "5":
-                return "fifteen";
-            case "6":
-                return "sixteen";
-            case "7":
-                return "seventeen";
-            case "8":
-                return "eighteen";
-            case "9":
-                return "nineteen";
-        }
-    } else {
-        $temp = convertDigit($digit2);
-        switch ($digit1) {
-            case "2":
-                return "twenty-$temp";
-            case "3":
-                return "thirty-$temp";
-            case "4":
-                return "forty-$temp";
-            case "5":
-                return "fifty-$temp";
-            case "6":
-                return "sixty-$temp";
-            case "7":
-                return "seventy-$temp";
-            case "8":
-                return "eighty-$temp";
-            case "9":
-                return "ninety-$temp";
-        }
-    }
-}
-
-function convertDigit($digit)
-{
-    switch ($digit) {
-        case "0":
-            return "zero";
-        case "1":
-            return "one";
-        case "2":
-            return "two";
-        case "3":
-            return "three";
-        case "4":
-            return "four";
-        case "5":
-            return "five";
-        case "6":
-            return "six";
-        case "7":
-            return "seven";
-        case "8":
-            return "eight";
-        case "9":
-            return "nine";
-    }
+    return $string;
 }
 
 ?>
