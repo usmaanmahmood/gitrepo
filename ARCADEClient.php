b671bf06f13b1ae3c402143399af132427f334e1
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-11-21 11:39:49 +0000

    script auto commit

--- ARCADEClient.php
+++ ARCADEClient.php
@@ -36,7 +36,7 @@ class ARCADEClient
         socket_write($socket, $this->arcadeUsername . "\n");
         socket_write($socket, $this->arcadePassword . "\n");
 
-        if (implode(' ', $inQuery->getDatabases()) ' ' || implode(' ', $inQuery->getDatabases()) '')
+        if (implode(' ', $inQuery->getDatabases()) ' ' || implode(' ', $inQuery->getDatabases()) == '')
             return 'ARCADE client error: No Databases sent through';
 
         $queryString = $inQuery->getCommand() . "\n"
