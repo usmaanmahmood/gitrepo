613e319faea50b252121fa6fc3c62dccacac6694
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-12-03 11:13:14 +0000

    script auto commit

--- ParsingScripts/MarksTableResult.php
+++ ParsingScripts/MarksTableResult.php
@@ -126,8 +126,8 @@ class MarksTableTable {
         while ($i < $arrayLength)
         {
             $this->marksTable[$i] = array($this->weightings[$i], $this->denominators[$i], $this->emailNames[$i], $this->marks[$i]);
-            $cleanMark = preg_replace("/[^0-9]+/", "", $this->marks[$i]);
-            $cleanDen = preg_replace("/[^0-9]+/", "", $this->denominators[$i]);
+            $cleanMark = (float)preg_replace("/[^0-9]+/", "", $this->marks[$i]);
+            $cleanDen = (float)preg_replace("/[^0-9]+/", "", $this->denominators[$i]);
 
             // add percentage
             if ($cleanMark != "" && $cleanDen != "")
