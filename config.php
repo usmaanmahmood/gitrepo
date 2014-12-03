e7c1bc754b3566d33eff281371cb07018d4961cc
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-12-03 09:30:37 +0000

    script auto commit

--- config.php
+++ config.php
@@ -2,18 +2,18 @@
 // Load the configuration file containing your database credentials
 //require_once('config.inc.php');
 // database stuff
-$database_host = 'dbhost.cs.man.ac.uk';
-$database_user = 'mmapxum2';
-$database_pass = 'manchester';
-$database_name = 'mmapxum2';
+//$database_host = 'dbhost.cs.man.ac.uk';
+//$database_user = 'mmapxum2';
+//$database_pass = 'manchester';
+//$database_name = 'mmapxum2';
 
 // Connect to the database
-$mysqli = new mysqli($database_host, $database_user, $database_pass, $database_name);
+//$mysqli = new mysqli($database_host, $database_user, $database_pass, $database_name);
 
 // Check for errors before doing anything else
-if ($mysqli->connect_error) {
-    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-}
+//if ($mysqli->connect_error) {
+//    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+//}
 
 // Always close your connection to the database cleanly!
 
