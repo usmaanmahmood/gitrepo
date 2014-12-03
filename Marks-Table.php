88c348226e8e7f838b93bef6e4caa0b7548f0aaa
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-12-03 10:07:18 +0000

    script auto commit

--- Marks-Table.php
+++ Marks-Table.php
@@ -36,21 +36,23 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="col-md-3">
-                <p>Modules &nbsp; | &nbsp;
-                    <button type="button" class="btn btn-default btn-xs reset-filters">Reset</button>
-                </p>
-                <select multiple class="form-control" size=10 id="ModuleList">
-                    <?php foreach (array_unique($arcadeProfile->getModuleList()) as $option) { ?>
-                        <option value="<?php echo $option ?>"><?php echo $option ?></option>
-                    <?php } ?>
-                </select>
+                <div class="col-md-12">
+                    <p>Modules &nbsp; | &nbsp;
+                        <button type="button" class="btn btn-default btn-xs reset-filters">Reset</button>
+                    </p>
+                    <select multiple class="form-control" size=10 id="ModuleList">
+                        <?php foreach (array_unique($arcadeProfile->getModuleList()) as $option) { ?>
+                            <option value="<?php echo $option ?>"><?php echo $option ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <div class="col-md-12">
+                    <button type="button" class="btn btn-default btn-lg btn-block" data-loading-text="Executing..."
+                            id="submit">Execute Query
+                    </button>
+                </div>
             </div>
-            <div class="col-md-3">
-                <button type="button" class="btn btn-default btn-lg btn-block" data-loading-text="Executing..."
-                        id="submit">Execute Query
-                </button>
-            </div>
-            <div id="result" class="col-md-6">
+            <div id="result" class="col-md-9">
             </div><!-- result -->
         </div>
     </div>
@@ -78,7 +80,6 @@
                 type: 'get',
                 success: function (result) {
                     $resultDiv.html(result);
-                    alert(result);
                     $resultDiv.fadeIn('slow');
                     $submitbutton.button('reset');
                 }
