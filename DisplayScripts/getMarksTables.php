ca119ae83e9fdd58719bbc68b10dac24e524eee1
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2014-12-14 16:13:21 +0000

    script auto commit

--- DisplayScripts/getMarksTables.php
+++ DisplayScripts/getMarksTables.php
@@ -76,6 +76,100 @@ $result = $arcadeClient->execute($query);
                     }?>
                 </tr>
             </table>
+<!--new table-->
+            <table class="table table-striped table-hover table-bordered table-condensed">
+                <tr>
+                    <th>Name</th>
+                    <th>Mark</th>
+                    <th>Weightings</th>
+                    <th>Deadline &percnt; of Coursework</th>
+                    <th>Your &percnt; of Coursework</th>
+                </tr>
+                <?php
+                $weightings = $table->getMarks();
+                $denominators = $table->getDenominators();
+                $percentageMarks = $table->getPercMark();
+                $percentageWeights = $table->getPercWeight();
+
+                foreach ($weightings as $key => $weighting) {
+                    echo "<tr>";
+
+                    // first
+                    $markString = "<td>" . $weighting;
+                    if (is_numeric($denominators[$key]))
+                        $markString = $markString . "/" . $denominators[$key]; // ignores last two cols
+
+                    if (!empty($percentages[$key]))
+                        $markString = $markString . " | " . $percentages[$key];
+
+                    $markString = $markString . "</td>";
+                    echo $markString;
+                    // end first
+                    //second
+                    echo "<td>" . $percentageMarks[$key] . "</td>";
+                    //end second
+                    // third
+                    echo "<td>" . $percentageWeights[$key] . "</td>";
+                    // end third
+
+                    echo "</tr>";
+                }
+
+
+                ?>
+
+                <tr>
+                    <td>
+
+                    </td>
+                </tr>
+
+
+                <tr>
+                    <th width="150px"></th>
+                    <?php foreach ($table->getEmailNames() as $weighting) {
+                        echo "<th>" . $weighting . "</th>";
+                    }?>
+                </tr>
+                <tr>
+                    <th width="150px">Marks</th>
+                    <?php
+                    $weightings = $table->getMarks();
+                    $denominators = $table->getDenominators();
+                    $percentages = $table->getPercMark();
+
+                    foreach ($weightings as $key => $weighting) {
+                        $markString = "<td>" . $weighting;
+                        if (is_numeric($denominators[$key]))
+                            $markString = $markString . "/" . $denominators[$key]; // ignores last two cols
+
+                        if (!empty($percentages[$key]))
+                            $markString = $markString . " | " . $percentages[$key];
+
+                        $markString = $markString . "</td>";
+                        echo $markString;
+                    }
+                    ?>
+                </tr>
+                <tr>
+                    <th width="150px">Weightings</th>
+                    <?php foreach ($table->getWeightings() as $weighting) {
+                        echo "<td>" . $weighting . "</td>";
+                    }?>
+                </tr>
+                <tr>
+                    <th width="150px">Deadline &percnt; of Coursework</th>
+                    <?php foreach ($table->getPercWeight() as $percentage) {
+                        echo "<td>" . $percentage . "</td>";
+                    }?>
+                </tr>
+                <tr>
+                    <th width="150px">Your &percnt; of Coursework</th>
+                    <?php foreach ($table->getPercScore() as $percentage) {
+                        echo "<td>" . $percentage . "</td>";
+                    }?>
+                </tr>
+            </table>
         <?php
         }
     }
