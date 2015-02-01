81b865f4a36694706d5e96618bc1e41521fa7005
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-02-01 14:39:23 +0000

    script auto commit

--- template-nav.php
+++ template-nav.php
@@ -15,40 +15,33 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li>
-                    <a href="Classic.php">Classic ARCADE</a>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">More Displays<span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="Registration-Details.php">Registration Details (+Display Picture)</a></li>
+                <li><a href="Classic.php">Classic ARCADE</a></li>
+                <li><a href="Registration-Details.php">Registration Details (+Display Picture)</a></li>
 <!--                        <li><a href="#">registration-details: with lab groups</a></li>-->
 <!--                        <li class="divider"></li>-->
 <!--                        <li><a href="#">display-picture</a></li>-->
-                        <li class="divider"></li>
-                        <li><a href="#">time-table: remaining</a></li>
-                        <li><a href="#">time-table: full</a></li>
-                        <li><a href="#">time-table: with extensions</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">attendance-summary</a></li>
-                        <li class="divider"></li>
-                        <li><a href="Marks-Table.php">marks-table: all</a></li>
-                        <li><a href="#">marks-table: fails only</a></li>
-                        <li><a href="#">marks-table: ignore empty columns</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">expected</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">excuses</a></li>
-                        <li class="divider"></li>
-                        <li><a href="Irregularities.php">Irregularities</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">failure-predictions</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">full-story: with notes</a></li>
-                        <li><a href="#">full-story: without notes</a></li>
-                        <li><a href="#">full-story: chronological</a></li>
-                    </ul>
-                </li>
+                <li class="divider"></li>
+                <li><a href="#">time-table: remaining</a></li>
+                <li><a href="#">time-table: full</a></li>
+                <li><a href="#">time-table: with extensions</a></li>
+                <li class="divider"></li>
+                <li><a href="#">attendance-summary</a></li>
+                <li class="divider"></li>
+                <li><a href="Marks-Table.php">marks-table: all</a></li>
+                <li><a href="#">marks-table: fails only</a></li>
+                <li><a href="#">marks-table: ignore empty columns</a></li>
+                <li class="divider"></li>
+                <li><a href="#">expected</a></li>
+                <li class="divider"></li>
+                <li><a href="#">excuses</a></li>
+                <li class="divider"></li>
+                <li><a href="Irregularities.php">Irregularities</a></li>
+                <li class="divider"></li>
+                <li><a href="#">failure-predictions</a></li>
+                <li class="divider"></li>
+                <li><a href="#">full-story: with notes</a></li>
+                <li><a href="#">full-story: without notes</a></li>
+                <li><a href="#">full-story: chronological</a></li>
                 <li>
                     <a href="Logout.php">Logout</a>
                 </li>
