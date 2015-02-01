73b7696837e64060413e9120af4303b13841f6c5
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-02-01 14:08:13 +0000

    script auto commit

--- template-head.php
+++ template-head.php
@@ -15,7 +15,7 @@ include 'config.php'; ?>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="author" content="">
-        <link href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.0/paper/bootstrap.min.css" rel="stylesheet">
+        <link href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.0/lumen/bootstrap.min.css" rel="stylesheet">
         <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
