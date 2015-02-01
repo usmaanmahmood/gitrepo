452a63e2a48d63f82dfb614085c088ce412054b6
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-02-01 13:31:45 +0000

    script auto commit

--- Irregularities.php
+++ Irregularities.php
@@ -54,7 +54,7 @@ foreach ($databaseList as $database) {
                 <h4 class="panel-title">
                     <a data-toggle="collapse" data-parent="#accordion"
                        href="#collapse<?= $currentNumber ?>"
-                       aria-expanded="true"
+                       aria-expanded="false"
                        aria-controls="collapse<?= $currentNumber ?>"><strong>
                             <?= $module; ?></strong></a>
                 </h4>
