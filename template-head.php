923d7fefd6829221b4b3daab3b8af4f98e223aa0
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-02-04 08:35:40 +0000

    script auto commit

--- template-head.php
+++ template-head.php
@@ -21,28 +21,3 @@ include 'config.php'; ?>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <script>
-        var themes = {
-            "default": "//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css",
-            "amelia": "//bootswatch.com/amelia/bootstrap.min.css",
-            "cerulean": "//bootswatch.com/cerulean/bootstrap.min.css",
-            "cosmo": "//bootswatch.com/cosmo/bootstrap.min.css",
-            "cyborg": "//bootswatch.com/cyborg/bootstrap.min.css",
-            "flatly": "//bootswatch.com/flatly/bootstrap.min.css",
-            "journal": "//bootswatch.com/journal/bootstrap.min.css",
-            "readable": "//bootswatch.com/readable/bootstrap.min.css",
-            "simplex": "//bootswatch.com/simplex/bootstrap.min.css",
-            "slate": "//bootswatch.com/slate/bootstrap.min.css",
-            "spacelab": "//bootswatch.com/spacelab/bootstrap.min.css",
-            "united": "//bootswatch.com/united/bootstrap.min.css"
-        };
-        $(function () {
-            var themesheet = $('<link href="' + themes['default'] + '" rel="stylesheet" />');
-            themesheet.appendTo('head');
-            $('.theme-link').click(function () {
-                var themeurl = themes[$(this).attr('data-theme')];
-                themesheet.attr('href', themeurl);
-            });
-        });</script>
