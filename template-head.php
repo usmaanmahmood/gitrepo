87d58c41d3328e38e73485180a881665b18d4e34
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-02-04 09:02:59 +0000

    script auto commit

--- template-head.php
+++ template-head.php
@@ -15,7 +15,7 @@ include 'config.php'; ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link href="//netdna.bootstrapcdn.com/bootswatch/3.0.0/lumen/bootstrap.min.css" rel="stylesheet" title="main">
+    <link href="//maxcdn.bootstrapcdn.com/bootswatch/3.0.0/lumen/bootstrap.min.css" rel="stylesheet" title="main">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
