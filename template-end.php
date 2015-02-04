b7643f24951508d8c98ed108a2eac52e01a11e7e
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-02-04 08:58:05 +0000

    script auto commit

--- template-end.php
+++ template-end.php
@@ -9,6 +9,30 @@ set_theme(theme);
 
 function set_theme(theme) {
 $('link[title="main"]').attr('href', theme);
+if (supports_storage) {
+localStorage.theme = theme;
+}
+}
+
+function supports_html5_storage() {
+try {
+return 'localStorage' in window && window['localStorage'] !== null;
+} catch (e) {
+return false;
+}
+}
+
+var supports_storage = supports_html5_storage();
+
+/* On load, set theme from local storage */
+if (supports_storage) {
+var theme = localStorage.theme;
+if (theme) {
+set_theme(theme);
+}
+} else {
+/* Don't annoy user with options that don't persist */
+$('#theme-dropdown').hide();
 }
 
 }) // document ready
