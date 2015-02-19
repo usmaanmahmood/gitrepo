bc1c8a99280cde3bb68f6e91b8da370f7cdd155f
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-02-19 12:51:27 +0000

    script auto commit

--- Marks-Table.php
+++ Marks-Table.php
@@ -87,7 +87,7 @@
             </select>
             <br/>
             <button type="button" class="btn btn-default btn-lg btn-block" data-loading-text="Executing..."
-                    id="submit">Execute!
+                    id="submit">Execute!111
             </button>
         </div>
 
