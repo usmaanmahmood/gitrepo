0c18853eec55cd48dbc1fa9d67be3f6ea17fe01c
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-02-22 15:27:29 +0000

    script auto commit

--- DisplayScripts/getIrregularitiesExcusesExpected.php
+++ DisplayScripts/getIrregularitiesExcusesExpected.php
@@ -20,67 +20,77 @@ $result = $arcadeClient->execute($query);
 
 ?>
 
-<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-    <?php
-    $databaseList = $result->getDatabaseList();
-    // database title
-    // new panel for each module
-
-    foreach ($databaseList as $database) {
-        ?>
-        <h3><?= $database->getDatabaseParsedName(); ?></h3>
+    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
         <?php
-        $itemModuleList = array_unique($database->getItemModuleList());
-        foreach ($itemModuleList as $key => $module) {
-            $key1 = $key + 1;
-            $currentNumber = ucfirst(convert_number_to_words(($key1)));
+        $databaseList = $result->getDatabaseList();
+        // database title
+        // new panel for each module
 
+        foreach ($databaseList as $database) {
             ?>
-            <div class="panel panel-default">
-                <div class="panel-heading" role="tab" id="heading<?= $currentNumber ?>">
-                    <h4 class="panel-title">
-                        <a data-toggle="collapse" data-parent="#accordion"
-                           href="#collapse<?= $currentNumber ?>"
-                           aria-expanded="true"
-                           aria-controls="collapse<?= $currentNumber ?>"><strong>
-                                <?= $module; ?></strong>
-                            - <?= $database->getItemCountForModule($module); ?> <?= $type ?></a>
-                    </h4>
-                </div>
-                <!-- panel-heading -->
-                <div id="collapse<?= $currentNumber ?>"
-                     class="panel-collapse collapse<?= $currentNumber == "One" ? " in" : "" ?>"
-                     role="tabpanel"
-                     aria-labelledby="heading<?= $currentNumber ?>">
-                    <!--                                <div class="panel-body">-->
-                    <table class="table table-striped table-hover">
-                        <tr>
-                            <th>Group:</th>
-                            <th>Date (DD/MM):</th>
-                            <th>Note:</th>
-                        </tr>
-                        <?php foreach ($database->getItemList() as $item) {
-                            if ($item->getModule() == $module) {
-                                ?>
-                                <tr>
-                                    <td><?= $item->getGroup(); ?></td>
-                                    <td><?= $item->getDate(); ?></td>
-                                    <td><?= $item->getNote(); ?></td>
-                                </tr>
-                            <?php
-                            } // if $module
-                        } // foreach $item in this module
-                        ?>
-                    </table>
-                    <!--                                </div>-->
-                </div>
-            </div> <!-- panel panel-default -->
-        <?php
-        } // foreach $module
-    } // foreach $database
-    ?>
-</div>
-<!-- panel-group -->
+            <h3><?= $database->getDatabaseParsedName(); ?></h3>
+            <?php
+            $itemModuleList = array_unique($database->getItemModuleList());
+            foreach ($itemModuleList as $key => $module) {
+                $key1 = $key + 1;
+                $currentNumber = ucfirst(convert_number_to_words(($key1)));
+
+                ?>
+                <div class="panel panel-default">
+                    <div class="panel-heading" role="tab" id="heading<?= $currentNumber ?>">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion"
+                               href="#collapse<?= $currentNumber ?>"
+                               aria-expanded="true"
+                               aria-controls="collapse<?= $currentNumber ?>"><strong>
+                                    <?= $module; ?></strong>
+                                - <?= $database->getItemCountForModule($module); ?> <?= $type ?></a>
+                        </h4>
+                    </div>
+                    <!-- panel-heading -->
+                    <div id="collapse<?= $currentNumber ?>"
+                         class="panel-collapse collapse<?= $currentNumber == "One" ? " in" : "" ?>"
+                         role="tabpanel"
+                         aria-labelledby="heading<?= $currentNumber ?>">
+                        <!--                                <div class="panel-body">-->
+                        <table class="table table-striped table-hover">
+                            <tr>
+                                <th>Group:</th>
+                                <th>Date (DD/MM):</th>
+                                <th>Note:</th>
+                            </tr>
+                            <?php foreach ($database->getItemList() as $item) {
+                                if ($item->getModule() == $module) {
+
+                                    $firstfive = substr($item->getNote(), 0, 5);
+                                    $current = false;
+                                    if ($firstfive != "(OLD)")
+                                        $current = true;
+                                    ?>
+                                    <tr
+                                        <?php
+                                        if ($current)
+                                            echo "class=\"danger\""
+                                        ?>
+                                        >
+                                        <td><?= $item->getGroup(); ?></td>
+                                        <td><?= $item->getDate(); ?></td>
+                                        <td><?= $item->getNote(); ?></td>
+                                    </tr>
+                                <?php
+                                } // if $module
+                            } // foreach $item in this module
+                            ?>
+                        </table>
+                        <!--                                </div>-->
+                    </div>
+                </div> <!-- panel panel-default -->
+            <?php
+            } // foreach $module
+        } // foreach $database
+        ?>
+    </div>
+    <!-- panel-group -->
 <?php
 function convert_number_to_words($number)
 {
