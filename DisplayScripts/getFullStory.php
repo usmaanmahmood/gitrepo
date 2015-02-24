86970dcb7a574f645e888102b6868ab468ccd027
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-02-24 15:47:23 +0000

    script auto commit

--- DisplayScripts/getFullStory.php
+++ DisplayScripts/getFullStory.php
@@ -31,7 +31,7 @@ $result = $arcadeClient->execute($query);
                 <td><?= $module->getModuleName(); ?> </td>
             </tr>
             <tr>
-                <td>
+                <td colspan="2">
                     <table class="table table-hover table-bordered table-condensed">
                         <tr>
                             <th>Session</th>
