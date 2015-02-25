b9166f184c96d2fd7e56e20e6c47aca1d3d6a849
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-02-25 19:07:40 +0000

    script auto commit

--- Marks-Table.php
+++ Marks-Table.php
@@ -17,61 +17,9 @@
 
     <div class="row">
         <div class="col-md-3">
-
-            <button type="button" class="btn btn-default btn-block dat-reset-filters">Reset Databases</button>
-
-            <select class="form-control" size=4 id="DatabaseList">
-                <?php foreach (array_unique($arcadeProfile->getDatabaseList()) as $option) { ?>
-                    <option value="<?php echo $option ?>">
-                        <?php preg_match("/(\d+)-(\d+)-(\d)(.*)/", $option, $matches);
-                        $databaseParsedName = ("Year " . $matches[3] . " - " . ($matches[4] == "X" ? ("Overall (inc. Exams)") : "Coursework Only"));
-                        echo $databaseParsedName; ?></option>
-                <?php } ?>
-            </select>
-            <br/>
-            <button type="button" class="btn btn-default btn-block mod-reset-filters">Reset Modules</button>
-
-
-            <select multiple class="form-control" size=15 id="ModuleList">
-                <?php $twoDArray = $arcadeProfile->getTwoDimensionalArray();
-                foreach ($twoDArray as $filter) {
-                    ?>
-                    <option value="<?php echo $filter[4] ?>" data-db="<?= $filter[0] ?>">
-                        <?php
-                        $matched = preg_match("/(\d{3,5})(\S)$/", $filter[4], $matches);
-
-                        if ($matched) {
-                            switch ($matches[2]) {
-                                case "L":
-                                    $matches[2] = " Labs";
-                                    break;
-                                case "C":
-                                    $matches[2] = " Coursework";
-                                    break;
-                                case "E":
-                                    $matches[2] = " Examples-Classes";
-                                    break;
-                                case "W":
-                                    $matches[2] = " Workshops";
-                                    break;
-                                case "X":
-                                    $matches[2] = " Exams";
-                                    break;
-                            }
-                        };
-
-                        if ($matched)
-                            echo $matches[1] . $matches[2];
-                        else
-                            echo $filter[4];
-                        ?></option>
-                <?php } ?>
-            </select>
-            <br/>
-            <button type="button" class="btn btn-default btn-lg btn-block" data-loading-text="Searching..."
-                    id="submit"><span class="glyphicon glyphicon-search"></span> Search
-            </button>
+            <?php include("SearchBarDatabaseModule.php"); ?>
         </div>
+
         <div class="col-md-9">
             <h1>Marks Table</h1>
             <div id="result"></div>
