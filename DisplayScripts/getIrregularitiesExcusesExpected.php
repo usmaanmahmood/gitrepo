ec17cd83c1e8b4414bd53a1bc43f1115661722ae
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-02-25 19:13:47 +0000

    script auto commit

--- DisplayScripts/getIrregularitiesExcusesExpected.php
+++ DisplayScripts/getIrregularitiesExcusesExpected.php
@@ -44,7 +44,7 @@ $result = $arcadeClient->execute($query);
                                aria-expanded="true"
                                aria-controls="collapse<?= $currentNumber ?>"><strong>
                                     <?= $module; ?></strong>
-                                - <span class="badge"><?= $database->getItemCountForModule($module); ?></span> <?= $type ?></a>
+                                - <span class="badge"><?= $database->getItemCountForModule($module); ?></span></a>
                         </h4>
                     </div>
                     <!-- panel-heading -->
