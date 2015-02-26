83bfb0977f85ba946a211116098c881c8d80b32a
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-02-26 14:09:16 +0000

    script auto commit

--- DisplayScripts/getAttendanceSummary.php
+++ DisplayScripts/getAttendanceSummary.php
@@ -20,64 +20,82 @@ if (!empty($_GET["modules"])) $query->addModules($_GET["modules"]);
 $result = $arcadeClient->execute($query);
 
 ?>
-<?php foreach($result->getDatabaseList() as $database)
-{
-?>
-<table class="table table-striped table-hover table-bordered table-condensed">
-    <tr>
-        <td>Database:</td><td><?=$database->getDatabaseParsedName();?></td>
-    </tr>
-    <tr>
-        <td>Group</td><td><?=$database->getGroup();?></td>
-    </tr>
-    <tr>
-        <td>Module List</td><td><?=implode(" ", $database->getModuleList());?></td>
-    </tr>
-    <tr>
-        <td>Percentage</td><td><?=$database->getPercentage();?>&percnt;</td>
-    </tr>
-    <tr>
-        <td>Pattern</td><td>
-    <?php
-    $rawPattern = $database->getAttendancePattern();
-    $splitPattern = str_split($rawPattern);
+<?php foreach ($result->getDatabaseList() as $database) {
+    ?>
+    <table class="table table-striped table-hover table-bordered table-condensed">
+        <tr>
+            <td>Database:</td>
+            <td><?= $database->getDatabaseParsedName(); ?></td>
+        </tr>
+        <tr>
+            <td>Group</td>
+            <td><?= $database->getGroup(); ?></td>
+        </tr>
+        <tr>
+            <td>Module List</td>
+            <td><?= implode(" ", $database->getModuleList()); ?></td>
+        </tr>
+        <tr>
+            <td>Percentage</td>
+            <td><?= $database->getPercentage(); ?>&percnt;</td>
+        </tr>
+        <tr>
+            <td>Pattern</td>
+            <td>
+                <button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Key:"
+                        data-content="<strong>Key:</strong><br />
+            One week per line<br />
+            <div class="green pull-left"></div> : attended<br />
+                <div class="orange pull-left"></div> : excused<br />
+                <div class="red pull-left"></div> : absent<br />
+                <div class="white pull-left"></div> : no attendance taken">
+                    Click to view Key
+                </button>
+                <?php
+                $rawPattern = $database->getAttendancePattern();
+                $splitPattern = str_split($rawPattern);
 
-    foreach($splitPattern as $character)
-    {
+                foreach ($splitPattern as $character) {
 
-        if ($character == "|") {
-            echo "<br /><div class=\"pull-left\">Semester Change</div><br />";
-            continue;
-        }
-        else if ($character == ".") {
-            echo "<br />";
-            continue;
-        }
+                    if ($character == "|") {
+                        echo "<br /><div class=\"pull-left\">Semester Change</div><br />";
+                        continue;
+                    } else if ($character == ".") {
+                        echo "<br />";
+                        continue;
+                    }
 
 //        echo $character;
 
-        $colour = "null";
-        switch ($character) {
-            case "/":   $colour = "green";
-                break;
-            case "x":   $colour = "red";
-                break;
-            case "E":   $colour = "orange";
-                break;
-            case "N":   $colour = "white";
-                break;
-            case "|":   $divider = "|";
-                break;
-            case ".":   $divider = ".";
-                break;
-        }
-        if ($colour != "null")
-            echo "<div class=\"" . $colour . " pull-left\"></div>";
-    }
-    ?></td>
-    </tr>
-    <tr>
-        <td>Raw Pattern</td><td><?=$database->getAttendancePattern();?></td>
-    </tr>
-</table>
+                    $colour = "null";
+                    switch ($character) {
+                        case "/":
+                            $colour = "green";
+                            break;
+                        case "x":
+                            $colour = "red";
+                            break;
+                        case "E":
+                            $colour = "orange";
+                            break;
+                        case "N":
+                            $colour = "white";
+                            break;
+                        case "|":
+                            $divider = "|";
+                            break;
+                        case ".":
+                            $divider = ".";
+                            break;
+                    }
+                    if ($colour != "null")
+                        echo "<div class=\"" . $colour . " pull-left\"></div>";
+                }
+                ?></td>
+        </tr>
+        <tr>
+            <td>Raw Pattern</td>
+            <td><?= $database->getAttendancePattern(); ?></td>
+        </tr>
+    </table>
 <?php } ?>
