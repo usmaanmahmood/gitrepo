b1cbb5b54161b92c23787f1197ed25944e82a9e6
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-03-01 19:38:35 +0000

    script auto commit

--- DisplayScripts/getMarksTables.php
+++ DisplayScripts/getMarksTables.php
@@ -89,6 +89,41 @@ $result = $arcadeClient->execute($query);
                 ?>
             </pre>
 
+
+
+            <Script>$('#highcharts').highcharts({
+
+                    title: {
+                        text: 'Global temperature change'
+                    },
+
+                    subtitle: {
+                        text: 'Data input from CSV'
+                    },
+
+                    data: {
+                        csv: document.getElementById('csv').innerHTML
+                    },
+
+                    plotOptions: {
+                        series: {
+                            marker: {
+                                enabled: false
+                            }
+                        }
+                    },
+
+                    series: [{
+                        lineWidth: 1
+                    }, {
+                        type: 'areaspline',
+                        color: '#c4392d',
+                        negativeColor: '#5679c4',
+                        fillOpacity: 0.5
+                    }]
+                });</Script>
+
+
         <?php
         }
     }
