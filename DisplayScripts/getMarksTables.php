81119277e4ee515da543ea91cf1d7edb750af5a5
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-03-01 20:03:08 +0000

    script auto commit

--- DisplayScripts/getMarksTables.php
+++ DisplayScripts/getMarksTables.php
@@ -91,6 +91,9 @@ $result = $arcadeClient->execute($query);
                 ?>
             </pre>
 
+            <script src="https://code.highcharts.com/highcharts.js"></script>
+            <script src="https://code.highcharts.com/modules/data.js"></script>
+            <script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script type="javascript">
 
 
