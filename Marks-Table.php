6ce3266da95fb890a182d266d4b0122af09669cc
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-03-01 18:37:48 +0000

    script auto commit

--- Marks-Table.php
+++ Marks-Table.php
@@ -80,7 +80,7 @@
 <script>
     $(document).ready(function () {
 
-        $(function () {
+
             $('#highcharts').highcharts({
                 title: {
                     text: 'Monthly Average Temperature',
@@ -127,7 +127,6 @@
                     data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
                 }]
             });
-        });
 
 
 
