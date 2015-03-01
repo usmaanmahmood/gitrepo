c6d788a3d70d27589bf51b69a7eab5f8740feb9c
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-03-01 20:03:27 +0000

    script auto commit

--- Marks-Table.php
+++ Marks-Table.php
@@ -42,6 +42,9 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/data.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script>
     $(document).ready(function () {
 
