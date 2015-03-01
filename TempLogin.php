cbf46f48ed7edfb8228d50f810d65ed4afa46117
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-03-01 20:48:44 +0000

    script auto commit

--- TempLogin.php
+++ TempLogin.php
@@ -2,7 +2,7 @@
 
 // username and password sent from form
 session_start();
-$username = $_POST['tempusername'];
-$arcadepassword = $_POST['arcadepassword'];
+$_SESSION["username"] = $_POST['tempusername'];;
+$_SESSION["arcadepassword"] = $_POST['arcadepassword'];
 header("location:Classic.php");
 ?>
