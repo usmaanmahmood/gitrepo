016e0180c31842af3795ba8570bb5f9a9ce44c86
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-03-01 12:10:44 +0000

    script auto commit

--- SearchBarDatabaseModule.php
+++ SearchBarDatabaseModule.php
@@ -1,4 +1,4 @@
-<button type="button" class="btn btn-default btn-block dat-reset-filters"><span class="glyphicon glyphicon-remove"></span> Reset Databases</button>
+<h2>Databases <button type="button" class="btn btn-default btn-s btn-warning dat-reset-filters pull-right"><span class="glyphicon glyphicon-remove"></span> Clear</button></h2>
 
 <select class="form-control" size=4 id="DatabaseList">
     <?php foreach (array_unique($arcadeProfile->getDatabaseList()) as $option) { ?>
@@ -9,8 +9,7 @@
     <?php } ?>
 </select>
 <br/>
-<h2>Modules
-    <button type="button" class="btn btn-default btn-s btn-warning mod-reset-filters pull-right"><span class="glyphicon glyphicon-remove"></span> Clear</button></h2>
+<h2>Modules <button type="button" class="btn btn-default btn-s btn-warning mod-reset-filters pull-right"><span class="glyphicon glyphicon-remove"></span> Clear</button></h2>
 
 
 <select multiple class="form-control" size=15 id="ModuleList">
