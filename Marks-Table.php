47169bcf9b3147f5db52cb523a879ee56c5b9558
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-03-01 18:57:41 +0000

    script auto commit

--- Marks-Table.php
+++ Marks-Table.php
@@ -130,6 +130,30 @@
                     $submitbutton.button('reset');
                 });
 
+            $('#container').highcharts({
+                data: {
+                    table: 'datatable'
+                },
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'Data extracted from a HTML table in the page'
+                },
+                yAxis: {
+                    allowDecimals: false,
+                    title: {
+                        text: 'Units'
+                    }
+                },
+                tooltip: {
+                    formatter: function () {
+                        return '<b>' + this.series.name + '</b><br/>' +
+                        this.point.y + ' ' + this.point.name.toLowerCase();
+                    }
+                }
+            });
+
 //            drawChart();
         });
         // } document ready in template end
