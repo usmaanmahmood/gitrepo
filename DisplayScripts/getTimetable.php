544465129a5ec9db18fecacdda27f08b01804ea4
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-03-01 16:17:09 +0000

    script auto commit

--- DisplayScripts/getTimetable.php
+++ DisplayScripts/getTimetable.php
@@ -57,7 +57,7 @@ $result = $arcadeClient->execute($query);
 
                         <span class="label label-success">Module: <?= $daySession->getModule() ?></span>
                         <span class="label label-info"><?= $daySession->getDateDay() . "/" . $daySession->getDateMonth() ?> <?php if($daySession->getTime() != "") echo "@" . $daySession->getTime() ?></span>
-
+                        <br/>
                         <span class="label label-default">Group: <?= $daySession->getGroup() ?></span>
                         <span class="label label-default">Info: <?= $daySession->getInfo() ?></span>
                         <br/><br/>
