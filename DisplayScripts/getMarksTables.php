5de3fe4fd59717ee814543b81ae28a7f1a0e6077
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-03-02 16:07:07 +0000

    script auto commit

--- DisplayScripts/getMarksTables.php
+++ DisplayScripts/getMarksTables.php
@@ -27,8 +27,7 @@ $result = $arcadeClient->execute($query);
         <div id="highcharts" style="min-width: 310px; height: 300px; margin: 20px auto 0px auto"></div>
         <?php
         foreach ($database->getTableList() as $table) {
-            echo "<h3>Table: " . $table->getName() . "</h3>";
-            echo "<h5>" . $table->getScalingFactor() . "</h5>";
+            echo "<h3>Table: " . $table->getName() . "<small>" . $table->getScalingFactor() . "</small></h3>";
             ?>
             <table class="table table-striped table-hover table-bordered table-condensed">
                 <tr>
