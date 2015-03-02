859bdc8e3c1e86a6be985367552bf36d0e4386bd
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-03-02 19:08:37 +0000

    script auto commit

--- Login.php
+++ Login.php
@@ -116,7 +116,7 @@
                            id="arcadepassword"
                            required value="LQKUGRIRDE">
                 </div>
-                copy ARCADE password to clipboard:<br/><pre>cat $HOME/.ARCADE/serverAuthentication | pbcopy</pre>
+                ARCADE password finder:<br/><pre>cat $HOME/.ARCADE/serverAuthentication</pre>
                 <br/>
                 <button class="btn btn-lg btn-primary btn-block" type="submit" id="submit"><span
                         class="glyphicon glyphicon-log-in"></span> Log in
