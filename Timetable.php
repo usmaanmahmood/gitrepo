2fb00bfb075efccd74e7f3970d45714dcd3e0bda
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-03-02 17:53:00 +0000

    script auto commit

--- Timetable.php
+++ Timetable.php
@@ -18,13 +18,13 @@
     <div class="row">
         <div class="col-md-3">
             <?php include("SearchBarDatabaseModule.php"); ?>
-            <button type="button" class="btn btn-default" data-loading-text="Searching..."
+            <button type="button" class="btn btn-default col-md-4" data-loading-text="Searching..."
                     id="submit"><span class="glyphicon glyphicon-search"></span> Search: Remaining
             </button>
-            <button type="button" class="btn btn-default" data-loading-text="Searching..."
+            <button type="button" class="btn btn-default col-md-4" data-loading-text="Searching..."
                     id="submit"><span class="glyphicon glyphicon-search"></span> Search: Full
             </button>
-            <button type="button" class="btn btn-default" data-loading-text="Searching..."
+            <button type="button" class="btn btn-default col-md-4" data-loading-text="Searching..."
                     id="submit"><span class="glyphicon glyphicon-search"></span> Search: Full + Extensions
             </button>
         </div>
