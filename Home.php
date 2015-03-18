f375fcc543a8d4535833ecba1bbbaffcd5dd54c0
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-03-18 11:58:20 +0000

    script auto commit

--- Home.php
+++ Home.php
@@ -18,17 +18,17 @@
             <div class="col-md-4">
                 <h2><span class="glyphicon glyphicon-heart"></span> Classic ARCADE</h2>
                 <p>A web replica of the existing JAVA GUI.</p>
-                <p><a class="btn btn-default" href="#" role="button">Go &raquo;</a></p>
+                <p><a class="btn btn-default btn-block" href="#" role="button">Go &raquo;</a></p>
             </div>
             <div class="col-md-4">
                 <h2><span class="glyphicon glyphicon-stats"></span> Attendance Summary</h2>
                 <p>A pattern of your attendance summary. Use it to get a rough idea of your attendance.</p>
-                <p><a class="btn btn-default" href="#" role="button">Go &raquo;</a></p>
+                <p><a class="btn btn-default btn-block" href="#" role="button">Go &raquo;</a></p>
             </div>
             <div class="col-md-4">
                 <h2><span class="glyphicon glyphicon-calendar"></span> Timetable</h2>
                 <p>Check your University timetable online.</p>
-                <p><a class="btn btn-default" href="#" role="button">Go &raquo;</a></p>
+                <p><a class="btn btn-default btn-block" href="#" role="button">Go &raquo;</a></p>
             </div>
         </div>
         <!-- Example row of columns -->
@@ -36,17 +36,17 @@
             <div class="col-md-4">
                 <h2><span class="glyphicon glyphicon-th-list"></span> Marks Tables</h2>
                 <p>View/print your marks across modules. Includes tables and graphs!</p>
-                <p><a class="btn btn-default" href="#" role="button">Go &raquo;</a></p>
+                <p><a class="btn btn-default btn-block" href="#" role="button">Go &raquo;</a></p>
             </div>
             <div class="col-md-4">
                 <h2><span class="glyphicon glyphicon-list-alt"></span> Full Story</h2>
                 <p>Includes specific attendance details, deadlines, marks. The whole lot.</p>
-                <p><a class="btn btn-default" href="#" role="button">Go &raquo;</a></p>
+                <p><a class="btn btn-default btn-block" href="#" role="button">Go &raquo;</a></p>
             </div>
             <div class="col-md-4">
                 <h2><span class="glyphicon glyphicon-education"></span> Registration Details</h2>
                 <p>All of the data ARCADE stores about you.</p>
-                <p><a class="btn btn-default" href="#" role="button">Go &raquo;</a></p>
+                <p><a class="btn btn-default btn-block" href="#" role="button">Go &raquo;</a></p>
             </div>
         </div>
         <!-- Example row of columns -->
@@ -54,17 +54,17 @@
             <div class="col-md-4">
                 <h2><span class="glyphicon glyphicon-question-sign"></span> Expected</h2>
                 <p>A list of what is expected from you.</p>
-                <p><a class="btn btn-default" href="#" role="button">Go &raquo;</a></p>
+                <p><a class="btn btn-default btn-block" href="#" role="button">Go &raquo;</a></p>
             </div>
             <div class="col-md-4">
                 <h2><span class="glyphicon glyphicon-info-sign"></span> Excuses</h2>
                 <p>A list of your excuses.</p>
-                <p><a class="btn btn-default" href="#" role="button">Go &raquo;</a></p>
+                <p><a class="btn btn-default btn-block" href="#" role="button">Go &raquo;</a></p>
             </div>
             <div class="col-md-4">
                 <h2><span class="glyphicon glyphicon-flag"></span> Irregularities</h2>
                 <p>A list of your irregularities.</p>
-                <p><a class="btn btn-default" href="#" role="button">Go &raquo;</a></p>
+                <p><a class="btn btn-default btn-block" href="#" role="button">Go &raquo;</a></p>
             </div>
         </div>
     </div>
