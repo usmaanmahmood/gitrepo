c229c2a7c638ab25db96f70c466d90b83264d08a
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-03-18 11:35:56 +0000

    script auto commit

--- index.php
+++ index.php
@@ -15,12 +15,9 @@
     <div class="container">
         <div class="jumbotron">
             <h1>Web ARCADE</h1>
-            <p>The new and improved ARCADE web app.</p>
+            <p>The solution to your ARCADE problems.</p>
             <p><a class="btn btn-primary btn-lg" href="Classic.php" role="button">Get started!</a></p>
         </div>
-    <p class="lead">What is this?</p>
-    <p>The existing JAVA ARCADE app has it's problems, as we all know. This is my solution.</p>
-    <p>Web ARCADE is my Third Year Project for my final year here in the School of Computer Science at the University of Manchester.</p>
     </div>
     <!-- /.container -->
 
