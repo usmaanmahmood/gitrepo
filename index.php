5e5539048623839de9176a0cdbffd3b34b77b381
Usmaan Mahmood <usmaanmahmood92@gmail.com>
2015-03-18 11:12:44 +0000

    script auto commit

--- index.php
+++ index.php
@@ -13,77 +13,10 @@
 
     <!-- Page Content -->
     <div class="container">
-
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>Classic ARCADE <small>A complete replica of the JAVA ARCADE Client!</small></h1>
-
-                <div class="row">
-                    <div class="col-md-4">
-                        <p class="text-center">Command</p>
-                        <select class="form-control" size=10 id="CommandList">
-                            <?php foreach ($arcadeProfile->getCommandList() as $command) { ?>
-                                <option><?php echo $command ?></option>
-                            <?php } ?>
-                        </select>
-
-                        <div class="col-md-12">
-                            <br />
-                            <div class=\"alert alert-danger\ role=\"alert\" id="queryWarning"></div>
-                        </div>
-                    </div>
-                    <div class="col-md-8">
-                        <div class="col-md-3">
-                            <p class="text-center">Databases
-                                <button type="button" class="btn btn-default btn-xs reset-filters pull-right">Reset</button>
-                            </p>
-                            <select multiple class="form-control" size=10 id="DatabaseList">
-                                <?php foreach (array_unique($arcadeProfile->getDatabaseList()) as $option) { ?>
-                                    <option value="<?php echo $option ?>"><?php echo $option ?></option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                        <div class="col-md-3">
-                            <p class="text-center">Groups
-                                <button type="button" class="btn btn-default btn-xs reset-filters pull-right">Reset</button>
-                            </p>
-                            <select multiple class="form-control" size=10 id="GroupList">
-                                <?php foreach (array_unique($arcadeProfile->getGroupList()) as $option) { ?>
-                                    <option value="<?php echo $option ?>"><?php echo $option ?></option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                        <div class="col-md-3">
-                            <p class="text-center">Students
-                                <button type="button" class="btn btn-default btn-xs reset-filters pull-right">Reset</button>
-                            </p>
-                            <select multiple class="form-control" size=10 id="StudentList">
-                                <?php foreach (array_unique($arcadeProfile->getStudentUsernameList()) as $option) { ?>
-                                    <option value="<?php echo $option ?>"><?php echo $option ?></option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                        <div class="col-md-3">
-                            <p class="text-center">Modules
-                                <button type="button" class="btn btn-default btn-xs reset-filters pull-right">Reset</button>
-                            </p>
-                            <select multiple class="form-control" size=10 id="ModuleList">
-                                <?php foreach (array_unique($arcadeProfile->getModuleList()) as $option) { ?>
-                                    <option value="<?php echo $option ?>"><?php echo $option ?></option>
-                                <?php } ?>
-                            </select>
-                        </div>
-                        <div class="col-md-12">
-                            <br />
-                            <button type="button" class="btn btn-default btn-lg btn-block" data-loading-text="Searching..."
-                                    id="submit"><span class="glyphicon glyphicon-search"></span> Search</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <pre id="resultspane"></pre>
-                </div>
-            </div>
+        <div class="jumbotron">
+            <h1>Hello, world!</h1>
+            <p>...</p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
         </div>
 
     </div>
